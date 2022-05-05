b54fc32fbc7a78e69a650f0fb735521d27251703
Mario Weber <mario@wondrous.ch>
2022-05-05 13:36:20 +0200

    debug sync

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,17 +22,17 @@ platform environment:info --project "$INPUT_PLATFORMSH_PROJECT" --environment "$
 
 #--- public files backup---
 echo -e "${red}Starting Public Files Backup...${reset}"
-rsync -azv --exclude 'styles' --exclude 'css' --exclude 'js' --exclude 'config_*' "$(platform ssh --pipe -e $INPUT_PLATFORMSH_ENVIRONMENT)":/app/"$INPUT_WITH_PUBLIC_FILES" ./"$INPUT_WITH_PUBLIC_FILES"/
+platform mount:download -e "$INPUT_PLATFORMSH_ENVIRONMENT" --target "$INPUT_PUBLIC_FILES_PATH" --mount "$INPUT_PUBLIC_FILES_PATH" --exclude 'styles' --exclude 'css' --exclude 'js' --exclude 'config_*' -y
+
 #todo upload
 echo -e "${green}Finished Public Files Backup...${reset}"
 #---private files backup---
-#if [ "$INPUT_WITH_PRIVATE_FILES" = 'yes' ]
-#then
-#  echo -e "${red}Starting Private Files Backup...${reset}"
-#  rsync -azv "$(platform ssh --pipe -e $INPUT_PLATFORMSH_ENVIRONMENT)":"/$INPUT_WITH_PRIVATE_FILES" ./"$INPUT_WITH_PRIVATE_FILES"/
-#  #todo upload
-#  echo -e "${green}Starting Private Files Backup...${reset}"
-#fi
+if [ "$INPUT_WITH_PRIVATE_FILES" = 'yes' ]
+then
+  echo -e "${red}Starting Private Files Backup...${reset}"
+  platform mount:download -e "$INPUT_PLATFORMSH_ENVIRONMENT" --target "$INPUT_PRIVATE_FILES_PATH" --mount "$INPUT_PRIVATE_FILES_PATH" --exclude 'twig' -y
+  echo -e "${green}Starting Private Files Backup...${reset}"
+fi
 
 #---repo backup---
 
