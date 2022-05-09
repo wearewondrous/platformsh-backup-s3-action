a06c70b06f02fb9a7534b2a354e2756a181ee87f
Mario Weber <mario@wondrous.ch>
2022-05-09 14:39:46 +0200

    enable all and do quiet

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,29 +16,30 @@ FILENAME_PRIVATE="backup-private-files-$DAY"
 FILENAME_SOURCE="backup-source-$DAY"
 
 #--- database backup---
-#echo -e "${red}Starting Database Backup...${reset}"
-#platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" --gzip -f "$FILENAME_DB".sql.gz
-#aws s3 cp "$FILENAME_DB".sql.gz s3://"$INPUT_AWS_S3_BUCKET"/"$DAY"
-#echo -e "${green}Finished Database Backup...${reset}"
+echo -e "${red}Starting Database Backup...${reset}"
+platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" --gzip -f "$FILENAME_DB".sql.gz
+aws s3 cp "$FILENAME_DB".zip s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/
+echo -e "${green}Finished Database Backup...${reset}"
 
 #--- public files backup---
-#echo -e "${red}Starting Public Files Backup...${reset}"
-#platform mount:download -e "$INPUT_PLATFORMSH_ENVIRONMENT" --target "$INPUT_PUBLIC_FILES_PATH" --mount "$INPUT_PUBLIC_FILES_PATH" --exclude 'styles' --exclude 'css' --exclude 'js' --exclude 'config_*' --exclude 'translations' -y
-##todo upload
-#echo -e "${green}Finished Public Files Backup...${reset}"
+echo -e "${red}Starting Public Files Backup...${reset}"
+platform mount:download -e "$INPUT_PLATFORMSH_ENVIRONMENT" --target "$INPUT_PUBLIC_FILES_PATH" --mount "$INPUT_PUBLIC_FILES_PATH" --exclude 'styles' --exclude 'css' --exclude 'js' --exclude 'config_*' --exclude 'translations' -y -q
+aws s3 cp "$FILENAME_PUBLIC".zip s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/ --quiet
+echo -e "${green}Finished Public Files Backup...${reset}"
 
 
 #---private files backup---
-#if [ "$INPUT_WITH_PRIVATE_FILES" = 'yes' ]
-#then
-#  echo -e "${red}Starting Private Files Backup...${reset}"
-#  platform mount:download -e "$INPUT_PLATFORMSH_ENVIRONMENT" --target "$INPUT_PRIVATE_FILES_PATH" --mount "$INPUT_PRIVATE_FILES_PATH" --exclude 'twig' -y
-#  echo -e "${green}Finished Private Files Backup...${reset}"
-#fi
+if [ "$INPUT_WITH_PRIVATE_FILES" = 'yes' ]
+then
+  echo -e "${red}Starting Private Files Backup...${reset}"
+  platform mount:download -e "$INPUT_PLATFORMSH_ENVIRONMENT" --target "$INPUT_PRIVATE_FILES_PATH" --mount "$INPUT_PRIVATE_FILES_PATH" --exclude 'twig' -y -q
+  aws s3 cp "$FILENAME_PRIVATE".zip s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/ --quiet
+  echo -e "${green}Finished Private Files Backup...${reset}"
+fi
 
 #---repo backup---
 echo -e "${red}Starting Source Backup...${reset}"
 git clone https://"$INPUT_GH_USER":"$GH_ACCESS_TOKEN"@github.com/"$INPUT_GH_REPOSITORY".git "$FILENAME_SOURCE"
 zip -r "$FILENAME_SOURCE".zip "$FILENAME_SOURCE"
-aws s3 cp "$FILENAME_SOURCE".zip s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/
+aws s3 cp "$FILENAME_SOURCE".zip s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/ --quiet
 echo -e "${green}Finished Private Files Backup...${reset}"
