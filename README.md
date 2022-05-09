a65bb3c2056de1a1f8f2f7efae122e22378ba97d
Mario Weber <mario@wondrous.ch>
2022-05-09 15:59:27 +0200

    cleanup and readme

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # platformsh-backup-s3-action
 
-wip
+## Setup
+Create workflow yml file and use the example below to configure the project.
+```$ touch .github/workflows/mybackups.yml```
+```yml
+name: Sync Backups
+on:
+  pull_request:
+jobs:
+  upload-backup:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - uses: wearewondrous/platformsh-backup-s3-action@v1
+        with:
+          project_name: 'my-project' # required.
+          gh_repository: ${{ github.repository }}  # required.
+          gh_user: 'myuser' # required.
+          platformsh_project: 'abcdefghijkl'  # required.
+          platformsh_environment: 'master'  # required.
+          aws_s3_bucket: 'my-bucket'  # required.
+          with_private_files: 'no' # optional default
+          public_files_path: 'web/sites/default/files' # optional default
+          private_files_path: 'private' # optional default
+        env:
+          PLATFORMSH_CLI_TOKEN: ${{ secrets.PLATFORMSH_CLI_TOKEN }}
+          GH_ACCESS_TOKEN: ${{ secrets.GH_ACCESS_TOKEN }}
+          AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
+          AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+          AWS_DEFAULT_REGION: ${{ secrets.AWS_DEFAULT_REGION }}
+```
+Set the required INPUT variables and add github secrets.
--- entrypoint.sh
+++ entrypoint.sh
@@ -16,34 +16,33 @@ FILENAME_PRIVATE="backup-private-files-$DAY"
 FILENAME_SOURCE="backup-source-$DAY"
 
 #--- database backup---
-#echo -e "${red}Starting Database Backup...${reset}"
-#platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" --gzip -f "$FILENAME_DB".sql.gz
-#aws s3 cp "$FILENAME_DB".sql.gz s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/ --quiet
-#echo -e "${green}Finished Database Backup...${reset}"
+echo -e "${red}Starting Database Backup...${reset}"
+platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" --gzip -f "$FILENAME_DB".sql.gz
+aws s3 cp "$FILENAME_DB".sql.gz s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/ --quiet
+echo -e "${green}Finished Database Backup...${reset}"
 
 #--- public files backup---
 echo -e "${red}Starting Public Files Backup...${reset}"
-platform mount:download -e "$INPUT_PLATFORMSH_ENVIRONMENT" --target "yyy" --mount "$INPUT_PUBLIC_FILES_PATH" --exclude 'styles' --exclude 'css' --exclude 'js' --exclude 'config_*' --exclude 'translations' -y
+platform mount:download -e "$INPUT_PLATFORMSH_ENVIRONMENT" --target "public-temp" --mount "$INPUT_PUBLIC_FILES_PATH" --exclude 'styles' --exclude 'css' --exclude 'js' --exclude 'config_*' --exclude 'translations' -y -q
 ls
-zip -r "$FILENAME_PUBLIC".zip "yyy" -q
+zip -r "$FILENAME_PUBLIC".zip "public-temp" -q
 aws s3 cp "$FILENAME_PUBLIC".zip s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/ --quiet
 echo -e "${green}Finished Public Files Backup...${reset}"
-
 
 #---private files backup---
 if [ "$INPUT_WITH_PRIVATE_FILES" = 'yes' ]
 then
   echo -e "${red}Starting Private Files Backup...${reset}"
-  platform mount:download -e "$INPUT_PLATFORMSH_ENVIRONMENT" --target "xxx" --mount "$INPUT_PRIVATE_FILES_PATH" --exclude 'twig' -y -q
+  platform mount:download -e "$INPUT_PLATFORMSH_ENVIRONMENT" --target "private-temp" --mount "$INPUT_PRIVATE_FILES_PATH" --exclude 'twig' -y -q
   ls
-  zip -r "$FILENAME_PRIVATE".zip "xxx" -q
+  zip -r "$FILENAME_PRIVATE".zip "private-temp" -q
   aws s3 cp "$FILENAME_PRIVATE".zip s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/ --quiet
   echo -e "${green}Finished Private Files Backup...${reset}"
 fi
 
 #---repo backup---
-#echo -e "${red}Starting Source Code Backup...${reset}"
-#git clone https://"$INPUT_GH_USER":"$GH_ACCESS_TOKEN"@github.com/"$INPUT_GH_REPOSITORY".git "$FILENAME_SOURCE" --quiet
-#zip -r "$FILENAME_SOURCE".zip "$FILENAME_SOURCE" -q
-#aws s3 cp "$FILENAME_SOURCE".zip s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/ --quiet
-#echo -e "${green}Finished Source Code Backup...${reset}"
+echo -e "${red}Starting Source Code Backup...${reset}"
+git clone https://"$INPUT_GH_USER":"$GH_ACCESS_TOKEN"@github.com/"$INPUT_GH_REPOSITORY".git "$FILENAME_SOURCE" --quiet
+zip -r "$FILENAME_SOURCE".zip "$FILENAME_SOURCE" -q
+aws s3 cp "$FILENAME_SOURCE".zip s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/ --quiet
+echo -e "${green}Finished Source Code Backup...${reset}"
