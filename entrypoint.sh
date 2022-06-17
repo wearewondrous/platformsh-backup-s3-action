13252fdacb516ecd1c3b839672641cd4274706b7
Tassilo Gröper <tassilogroeper@wearewondrous.com>
2022-06-17 17:34:32 +0200

    use env vars and default vars to create folders and file names Refactor redundant vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,40 +9,46 @@ platform --version
 git --version
 
 sed -i 's/#   StrictHostKeyChecking ask.*/StrictHostKeyChecking accept-new/' /etc/ssh/ssh_config
-DAY=$(date +%F-%T)
-FILENAME_DB="backup-db-$DAY"
-FILENAME_PUBLIC="backup-public-files-$DAY"
-FILENAME_PRIVATE="backup-private-files-$DAY"
-FILENAME_SOURCE="backup-source-$DAY"
 
-#--- database backup---
+#--- set variables ---
+REPOSITORY_NAME=$(echo $GITHUB_REPOSITORY | awk -F '/' '{print $2}')
+DAY=$(date +%F-%T)
+S3_URI="s3://$INPUT_AWS_S3_BUCKET/$REPOSITORY_NAME/$DAY/"
+
+FILE_PREFIX="$REPOSITORY_NAME--$GITHUB_REF_NAME--$DAY"
+FILENAME_DB="$FILE_PREFIX--database"
+FILENAME_PUBLIC="$FILE_PREFIX--public-files"
+FILENAME_PRIVATE="$FILE_PREFIX--private-files"
+FILENAME_SOURCE="$FILE_PREFIX--source-code"
+
+#--- database backup ---
 echo -e "${red}Starting Database Backup...${reset}"
-platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$INPUT_PLATFORMSH_ENVIRONMENT" --gzip -f "$FILENAME_DB".sql.gz
-aws s3 cp "$FILENAME_DB".sql.gz s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/ --quiet
+platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$GITHUB_REF_NAME" --gzip -f "$FILENAME_DB".sql.gz
+aws s3 cp "$FILENAME_DB".sql.gz "$S3_URI" --quiet
 echo -e "${green}Finished Database Backup...${reset}"
 
-#--- public files backup---
+#--- public files backup ---
 echo -e "${red}Starting Public Files Backup...${reset}"
-platform mount:download -e "$INPUT_PLATFORMSH_ENVIRONMENT" --target "public-temp" --mount "$INPUT_PUBLIC_FILES_PATH" --exclude 'styles' --exclude 'css' --exclude 'js' --exclude 'config_*' --exclude 'translations' -y -q
+platform mount:download -e "$GITHUB_REF_NAME" --target "public-temp" --mount "$INPUT_PUBLIC_FILES_PATH" --exclude 'styles' --exclude 'css' --exclude 'js' --exclude 'config_*' --exclude 'translations' -y -q
 ls
 zip -r "$FILENAME_PUBLIC".zip "public-temp" -q
-aws s3 cp "$FILENAME_PUBLIC".zip s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/ --quiet
+aws s3 cp "$FILENAME_PUBLIC".zip "$S3_URI" --quiet
 echo -e "${green}Finished Public Files Backup...${reset}"
 
-#---private files backup---
-if [ "$INPUT_WITH_PRIVATE_FILES" = 'yes' ]
+#--- private files backup ---
+if [[ -d "$INPUT_PRIVATE_FILES_PATH" ]]
 then
   echo -e "${red}Starting Private Files Backup...${reset}"
-  platform mount:download -e "$INPUT_PLATFORMSH_ENVIRONMENT" --target "private-temp" --mount "$INPUT_PRIVATE_FILES_PATH" --exclude 'twig' -y -q
+  platform mount:download -e "$GITHUB_REF_NAME" --target "private-temp" --mount "$INPUT_PRIVATE_FILES_PATH" --exclude 'twig' -y -q
   ls
   zip -r "$FILENAME_PRIVATE".zip "private-temp" -q
-  aws s3 cp "$FILENAME_PRIVATE".zip s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/ --quiet
+  aws s3 cp "$FILENAME_PRIVATE".zip "$S3_URI" --quiet
   echo -e "${green}Finished Private Files Backup...${reset}"
 fi
 
-#---repo backup---
+#--- repo backup ---
 echo -e "${red}Starting Source Code Backup...${reset}"
 git clone https://"$INPUT_GH_USER":"$GH_ACCESS_TOKEN"@github.com/"$INPUT_GH_REPOSITORY".git "$FILENAME_SOURCE" --quiet
 zip -r "$FILENAME_SOURCE".zip "$FILENAME_SOURCE" -q
-aws s3 cp "$FILENAME_SOURCE".zip s3://"$INPUT_AWS_S3_BUCKET"/"$INPUT_PROJECT_NAME"/"$DAY"/ --quiet
+aws s3 cp "$FILENAME_SOURCE".zip "$S3_URI" --quiet
 echo -e "${green}Finished Source Code Backup...${reset}"
