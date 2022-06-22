e0fc7fcaed04cbf4951962e34d28bbbf049932a6
Tassilo Gröper <tassilogroeper@wearewondrous.com>
2022-06-22 10:20:20 +0200

    use target branch var and align platform cli calls

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,26 +17,22 @@ DATETIME=$(date +%FT%T)
 S3_BASE_URI="s3://$INPUT_AWS_S3_BUCKET/$REPOSITORY_NAME"
 S3_BACKUP_URI="$S3_BASE_URI/$DATETIME/"
 
-FILE_PREFIX="$REPOSITORY_NAME--$GITHUB_REF_NAME--$DATETIME"
+FILE_PREFIX="$REPOSITORY_NAME--$INPUT_TARGET_BRANCH--$DATETIME"
 FILENAME_DB="$FILE_PREFIX--database"
 FILENAME_PUBLIC="$FILE_PREFIX--public-files"
 FILENAME_PRIVATE="$FILE_PREFIX--private-files"
 FILENAME_SOURCE="$FILE_PREFIX--source-code"
 
-#--- test ---
-echo "GITHUB_REF_NAME is $GITHUB_REF_NAME"
-ls
-git checkout
-
+PLATFORM_CLI_DEFAULT_ARGS="--yes --quiet --project $INPUT_PLATFORMSH_PROJECT --environment $INPUT_TARGET_BRANCH"
 #--- database backup ---
 echo -e "${yellow}Starting Database Backup...${reset}"
-platform db:dump -v --yes --project "$INPUT_PLATFORMSH_PROJECT" --environment "$GITHUB_REF_NAME" --gzip -f "$FILENAME_DB".sql.gz -q
+platform db:dump "$PLATFORM_CLI_DEFAULT_ARGS" --gzip -f "$FILENAME_DB".sql.gz
 aws s3 cp "$FILENAME_DB".sql.gz "$S3_BACKUP_URI" --only-show-errors
 echo -e "${green}Finished Database Backup...${reset}"
 
 #--- public files backup ---
 echo -e "${yellow}Starting Public Files Backup...${reset}"
-platform mount:download --environment "$GITHUB_REF_NAME" --target "public-temp" --mount "$INPUT_PUBLIC_FILES_PATH" --exclude 'styles' --exclude 'css' --exclude 'js' --exclude 'config_*' --exclude 'translations' -y -q
+platform mount:download "$PLATFORM_CLI_DEFAULT_ARGS" --target "public-temp" --mount "$INPUT_PUBLIC_FILES_PATH" --exclude 'styles' --exclude 'css' --exclude 'js' --exclude 'config_*' --exclude 'translations'
 zip -r "$FILENAME_PUBLIC".zip "public-temp" -q
 aws s3 cp "$FILENAME_PUBLIC".zip "$S3_BACKUP_URI" --only-show-errors
 echo -e "${green}Finished Public Files Backup...${reset}"
@@ -45,7 +41,7 @@ echo -e "${green}Finished Public Files Backup...${reset}"
 if [[ -d "$INPUT_PRIVATE_FILES_PATH" ]]
 then
   echo -e "${yellow}Starting Private Files Backup...${reset}"
-  platform mount:download --environment "$GITHUB_REF_NAME" --target "private-temp" --mount "$INPUT_PRIVATE_FILES_PATH" --exclude 'twig' -y -q
+  platform mount:download "$PLATFORM_CLI_DEFAULT_ARGS" --target "private-temp" --mount "$INPUT_PRIVATE_FILES_PATH" --exclude 'twig'
   zip -r "$FILENAME_PRIVATE".zip "private-temp" -q
   aws s3 cp "$FILENAME_PRIVATE".zip "$S3_BACKUP_URI" --only-show-errors
   echo -e "${green}Finished Private Files Backup...${reset}"
@@ -56,7 +52,7 @@ echo -e "${yellow}Starting Source Code Backup...${reset}"
 
 REMOTE_REPO_URL="https://$INPUT_GH_USER:$GH_ACCESS_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 
-git clone --branch "$GITHUB_REF_NAME" "$REMOTE_REPO_URL" "$FILENAME_SOURCE" --quiet
+git clone --branch "$INPUT_TARGET_BRANCH" "$REMOTE_REPO_URL" "$FILENAME_SOURCE" --quiet
 zip -r "$FILENAME_SOURCE".zip "$FILENAME_SOURCE" -q
 aws s3 cp "$FILENAME_SOURCE".zip "$S3_BACKUP_URI" --only-show-errors
 echo -e "${green}Finished Source Code Backup...${reset}"
